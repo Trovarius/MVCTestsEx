10cd7c4f85827b92ba14ded07ad14e756e036058
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2010-11-29 23:18:33 -0200

    removeng unused nunit references, adding ASP.NET MVC reference, adding first test

--- src/MVCTestsEx/MVCTestsEx/ViewResultExtesionsTests.cs
+++ src/MVCTestsEx/MVCTestsEx/ViewResultExtesionsTests.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
 
 namespace MVCTestsEx
 {
+    [TestFixture]
     public class ViewResultExtesionsTests
     {
+        [Test]
+        public void testing_that_AssertViewName_passing_the_correct_viewresult_viewname_should_pass()
+        {
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "SomeName";
+
+            viewResult.AssertViewName("SomeName");
+        }
+    }
+
+    public static class MVCTestsExtensions
+    {
+        public static void AssertViewName(this ViewResult viewResult, string expectedViewName)
+        {
+            Assert.AreEqual(expectedViewName, viewResult.ViewName);
+        }
     }
 }
