a8df3709e4f3b7ea1676f2f5a8fb999b5b9f9306
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2010-11-30 00:23:41 -0200

    moving MVCTestsExtensions to a separated file

---
+++ src/MVCTestsEx/MVCTestsEx/MVCTestsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+
+namespace MVCTestsEx
+{
+    public static class MVCTestsExtensions
+    {
+        public static void AssertViewName(this ViewResult viewResult, string expectedViewName)
+        {
+            Assert.AreEqual(expectedViewName, viewResult.ViewName);
+        }
+    }
+}
